c2ae8f3ca5863e87ed2a326daeb788b64056a770
laplace0606 <jesusgareyes@gmail.com>
2025-06-16 18:27:05 -0400

    cajero definitivo con conflictos resuelto

--- cajero.py
+++ cajero.py
@@ -1,10 +1,13 @@
 class cuenta :
-    def __init__(self,saldo = 1000 ):
+    def __init__(self,saldo = 1000, id = 102381283180 ):
      self.saldo = saldo
-    #mostrar
-    def mostrar():
-       print (f"su saldo es: $"{self.saldo})
-    #agregar saldo
+     self.id = id
+     
+    #mostrar saldo
+    def mostrar(self):
+       print (f"numero de cuenta: {self.id}")
+       print (f"su saldo es: ${self.saldo}")
+    #mostrar saldo
     def agregar(self):
        esmeralda = input("ingrese su saldo: ")
        self.saldo += esmeralda
@@ -19,4 +22,22 @@ class cuenta :
             else:
                 self.saldo -= rubi
                 break
-    
+caja = cuenta()
+while True:
+   print("bievenido a su cajero de confianza")
+   print("que desea hacer ?")
+   print("presione 1 si usted desea consultar saldo y numero de cuenta")
+   print("2 si quiere agregar saldo")
+   print("3 si quiere retirar saldo")
+   print("presione cualquier otro boton para salir")
+   numero = input("incertar numero: ")
+   
+   if numero == 1:
+        caja.mostrar()
+   elif numero == 2:
+       caja.agregar()
+   elif numero == 3:
+       caja.retirar()
+   else:
+       print("hasta pronto :)")
+       break
