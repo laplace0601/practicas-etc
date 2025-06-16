80d775ff3439485b9f8bb2c712910a467320c223
laplace0606 <jesusgareyes@gmail.com>
2025-06-16 14:11:58 -0400

    elimine carpeta inecesaria y agregue una clase de cajero de banco y un funcion que crea una flor

---
+++ cajero.py
@@ -0,0 +1,20 @@
+class cuenta :
+    def __init__(self,saldo = 1000 ):
+     self.saldo = saldo
+    #mostrar
+    def mostrar():
+       print (f"su saldo es: $"{self.saldo})
+
+    def agregar(self):
+       esmeralda = input("ingrese su saldo: ")
+       self.saldo += esmeralda
+    def retirar(self):
+        while True:
+            rubi = input("ingrese valor para retirar: ")
+
+            if rubi > self.saldo:
+                print("su valor seleccionado excede la cantidad de su cuenta")
+                break
+            else:
+                self.saldo -= rubi
+                break
