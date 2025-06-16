00111ee5f7349a94e1ed9c6516783ada091b7b74
laplace0606 <jesusgareyes@gmail.com>
2025-06-16 14:31:09 -0400

    se agrego una nueva frase a la ruleta rusa cuando dispara

--- ruletarusa.py
+++ ruletarusa.py
@@ -2,12 +2,14 @@ import random
 def juego():
     tambor = [False] * 5 + [True]
     random.shuffle(tambor)
+    print("BIENVENIDO A LA RULETA RUSA")
     while tambor:
-        print("BIENVENIDO A LA RULETA RUSA")
+        
         print("presiona enter para disparar")
         intento = input()
         if tambor.pop(0): 
-          print(f"BANG MUELTO")
+          print("PEEEEEEEEEERSOOOOOOOOOOOOOONAAAAAAAAAAAAAAAAAAAA")
+          print("BANG")
           break
         else:
          print("salvado la proxima no la cuentas")
