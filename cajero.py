534d7d5cdd00dece9b11e3bc04402fedc2599d88
laplace0606 <jesusgareyes@gmail.com>
2025-06-16 18:32:11 -0400

    resolucion a problemas de git

--- cajero.py
+++ cajero.py
@@ -7,37 +7,39 @@ class cuenta :
     def mostrar(self):
        print (f"numero de cuenta: {self.id}")
        print (f"su saldo es: ${self.saldo}")
-    #mostrar saldo
     def agregar(self):
-       esmeralda = input("ingrese su saldo: ")
+       esmeralda = float(input("ingrese su saldo: "))
        self.saldo += esmeralda
-    #retirar saldo
+       # retirar saldo
     def retirar(self):
         while True:
-            rubi = input("ingrese valor para retirar: ")
-
+            rubi = float(input("ingrese valor para retirar: "))
+        #condicional para evitar que saquen mas saldo de lo que ya se tiene
             if rubi > self.saldo:
                 print("su valor seleccionado excede la cantidad de su cuenta")
                 break
             else:
                 self.saldo -= rubi
                 break
+    
 caja = cuenta()
-while True:
-   print("bievenido a su cajero de confianza")
-   print("que desea hacer ?")
-   print("presione 1 si usted desea consultar saldo y numero de cuenta")
-   print("2 si quiere agregar saldo")
-   print("3 si quiere retirar saldo")
-   print("presione cualquier otro boton para salir")
-   numero = input("incertar numero: ")
-   
-   if numero == 1:
-        caja.mostrar()
-   elif numero == 2:
-       caja.agregar()
-   elif numero == 3:
-       caja.retirar()
-   else:
-       print("hasta pronto :)")
-       break
+#interfaz del banco
+def cajabank():
+    while True:
+        print("bievenido a su cajero de confianza")
+        print("que desea hacer ?")
+        print("presione 1 si usted desea consultar saldo y numero de cuenta")
+        print("2 si quiere agregar saldo")
+        print("3 si quiere retirar saldo")
+        print("presione cualquier otro boton para salir")
+        numero = float(input("incertar numero: "))
+    
+        if numero == 1:
+                caja.mostrar()
+        elif numero == 2:
+            caja.agregar()
+        elif numero == 3:
+            caja.retirar()
+        else:
+            print("hasta pronto :)")
+            break
